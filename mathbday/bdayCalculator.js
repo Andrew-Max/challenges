88698e147aa4146285f5486f43f63783daaa42e5
Andrew Max <anm898989@gmail.com>
2017-04-24 22:10:42 -0400

    updated mbdc styling and ouput

--- mathbday/bdayCalculator.js
+++ mathbday/bdayCalculator.js
@@ -5,33 +5,37 @@ $( document ).ready(function() {
     $('.input-container button').on('click', function () {
 		var bdayInput = $('.input-container input').val();
 		var bdayObj = calculateMathBDay(bdayInput);
-		debugger;
-		$('.result-container span.next-bday').html(bdayObj.dayOfNextMathBirthday);
-		$('.result-container span.days-till').html(bdayObj.daysTillNextMathBirthday);
 
+		$('.result-container').show();
+		$('.result-container span.next-bday-days').html(bdayObj.dayOfNextMathBDay);
+		$('.result-container span.days-till').html(bdayObj.daysTillNextMathBDay);
+		$('.result-container span.next-bday-date').html(bdayObj.nextMathBDayDate);
 	});
-
 });
 
 function calculateMathBDay(date) {
   var birthday = moment(date);
   var numCurrentDays = moment().diff(birthday, "days");
   var currentDaysCharLength = numCurrentDays.toString().length;
-  var dayOfNextMathBirthday = Math.pow(10, currentDaysCharLength);
+  var dayOfNextMathBDay = Math.pow(10, currentDaysCharLength);
+  var daysTillNextMathBDay = dayOfNextMathBDay - numCurrentDays;
+  var nextMathBDayDate = moment().add(daysTillNextMathBDay, 'days');
+  nextMathBDayDate = nextMathBDayDate.format("dddd, MMMM Do YYYY");
   
   return {
-	dayOfNextMathBirthday: dayOfNextMathBirthday,
-	daysTillNextMathBirthday: dayOfNextMathBirthday - numCurrentDays,
+	dayOfNextMathBDay: dayOfNextMathBDay,
+	daysTillNextMathBDay: daysTillNextMathBDay,
+	nextMathBDayDate: nextMathBDayDate
   }
 };
 
-function  handleBdayInput() {
-	return function () {
-		var bdayInput = $('.input-container input').val
-		var bdayObj = calculateMathBDay(bdayInput);
-		$('.result-container span.days').html(bdayObj.dayOfNextMathBirthday)
-	};
-}
+// function  handleBdayInput() {
+// 	return function () {
+// 		var bdayInput = $('.input-container input').val
+// 		var bdayObj = calculateMathBDay(bdayInput);
+// 		$('.result-container span.days').html(bdayObj.dayOfNextMathBirthday)
+// 	};
+// }
 
 function initializeDatepicker() {
 	$("#datepicker").datepicker({
