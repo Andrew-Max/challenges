9f3796460690d1ac0476f20f96b5612174177948
Andrew Max <anm898989@gmail.com>
2017-04-25 01:25:03 -0400

    mbdc calculations for extra data

--- mathbday/bdayCalculator.js
+++ mathbday/bdayCalculator.js
@@ -1,3 +1,6 @@
+//=============================
+// Main function
+//=============================
 $( document ).ready(function() {
     initializeDatepicker();
     $('.input-container button').on('click', handleBdayInput);
@@ -5,22 +8,111 @@ $( document ).ready(function() {
 
 });
 
-function calculateMathBDay(date) {
-  var birthday = moment(date, 'MM-DD-YYYY');
-  var numCurrentDays = moment().diff(birthday, "days");
-  var currentDaysCharLength = numCurrentDays.toString().length;
-  var dayOfNextMathBDay = Math.pow(10, currentDaysCharLength);
-  var daysTillNextMathBDay = dayOfNextMathBDay - numCurrentDays;
-  var nextMathBDayDate = moment().add(daysTillNextMathBDay, 'days');
-  nextMathBDayDate = nextMathBDayDate.format("dddd, MMMM Do YYYY");
-  
-  return {
-	dayOfNextMathBDay: dayOfNextMathBDay,
-	daysTillNextMathBDay: daysTillNextMathBDay,
-	nextMathBDayDate: nextMathBDayDate
-  };
+
+//=============================
+//Data Caculation
+//=============================
+function calcReturnData(date) {
+	// turn to actual object
+	var returnObj = {}
+
+	var presetsObj = createPresetsObj(date);
+	returnObj.nextBDay = calcNextMathBDay(presetsObj);
+	returnObj.lastBDay = calcLastMathBDay(presetsObj);
+	returnObj.extras = calcExtras(presetsObj);
+	return returnObj;
 };
 
+function createPresetsObj(date) {
+	// turn to actual object
+	var presetsObj = {
+		date: date,
+		birthday: function() {
+			return moment(this.date, 'MM-DD-YYYY')
+		},
+		numCurrentDays: function () {
+			return moment().diff(this.birthday(), "days")
+		},
+		currentDaysCharLength: function () {
+			return this.numCurrentDays().toString().length;
+		}
+	};
+
+	return presetsObj;
+};
+
+function calcNextMathBDay(presetsObj) {
+  // turn to actual object
+  var nextMathBDayObj = {
+	dayOfNextMathBDay: function() {
+		return Math.pow(10, presetsObj.currentDaysCharLength());
+	},
+	daysTillNextMathBDay: function() {
+		debugger;
+		return this.dayOfNextMathBDay() - presetsObj.numCurrentDays();
+	},
+	nextMathBDayDate: function() {
+		var nextMathBDayDate = moment().add(this.daysTillNextMathBDay(), 'days');
+		return nextMathBDayDate.format("dddd, MMMM Do YYYY");
+	}
+  };
+
+  return nextMathBDayObj;
+};
+
+function calcLastMathBDay(presetsObj) {
+	var lastMathBDayObj = {
+		dayOfLastMathBDay: function() {
+			return Math.pow(10, presetsObj.currentDaysCharLength() - 1);
+		},
+		daysSinceLastMathBDay: function() {
+			return presetsObj.numCurrentDays() - this.dayOfLastMathBDay();
+		},
+		lastMathBDayDate: function () {
+			var lastMathBDayDate = moment().subtract(daysSinceLastMathBDay, 'days');
+			return lastMathBDayDate.format("dddd, MMMM Do YYYY");
+		}
+	};
+
+	return lastMathBDayObj;
+};
+
+function calcExtras(presetsObj) {
+	var extrasObj = {
+		currentDays: function() {
+			return presetsObj.numCurrentDays();
+		},
+		BDay10: function() {
+			return calcTimeDiffString(presetsObj, 10);
+		},
+		BDay100: function() {
+			return calcTimeDiffString(presetsObj, 100);
+		},
+		BDay1000: function() {
+			return calcTimeDiffString(presetsObj, 1000);
+		},
+		BDay10000: function() {
+			return calcTimeDiffString(presetsObj, 10000);
+		},
+		BDay100000: function() {
+			return calcTimeDiffString(presetsObj, 100000);
+		},
+	}
+
+	return extrasObj;
+};
+
+function calcTimeDiffString(presetsObj, bday) {
+	var date = presetsObj.birthday().add(bday, "days");
+	var isBeforeBirthday = date.isBefore(moment());
+	var timeString = isBeforeBirthday ? "was " + date.fromNow() : "is " + date.fromNow();
+	return "Your " + bday.toString() + " day Math Birthday " + timeString + " on " + date.format("dddd, MMMM Do YYYY");
+		"dddd, MMMM Do YYYY";
+}
+
+//=============================
+// Initialization
+//=============================
 function initializeDatepicker() {
 	$("#datepicker").datepicker({
 	    changeMonth: true,
@@ -30,6 +122,10 @@ function initializeDatepicker() {
 	});
 };
 
+
+//=============================
+// Event Handlers
+//=============================
 function handleBdayInput() {
 	var bdayInput = $('.input-container input').val();
 
@@ -39,12 +135,18 @@ function handleBdayInput() {
 	    clearErrors();
 	}
 
-	var bdayObj = calculateMathBDay(bdayInput);
+	var dataObj = calcReturnData(bdayInput);
+	console.log(dataObj.extras.BDay10())
+	console.log(dataObj.extras.BDay100())
+	console.log(dataObj.extras.BDay1000())
+	console.log(dataObj.extras.BDay10000())
+	console.log(dataObj.extras.BDay100000())
+
 
 	$('.result-container').show();
-	$('.result-container span.next-bday-days').html(bdayObj.dayOfNextMathBDay);
-	$('.result-container span.days-till').html(bdayObj.daysTillNextMathBDay);
-	$('.result-container span.next-bday-date').html(bdayObj.nextMathBDayDate);
+	$('.result-container span.next-bday-days').html(dataObj.dayOfNextMathBDay);
+	$('.result-container span.days-till').html(dataObj.daysTillNextMathBDay);
+	$('.result-container span.next-bday-date').html(dataObj.nextMathBDayDate);
 };
 
 function handleInputReset() {
@@ -53,11 +155,19 @@ function handleInputReset() {
 	clearErrors();
 };
 
+
+//=============================
+//Errors
+//=============================
 function throwNoInputError() {
 	$('.errors-container').show();
 	$('.errors-container .error').html('Please Choose A Date Before Submitting');
 };
 
+
+//=============================
+//Utility
+//=============================
 function clearErrors() {
 	$('.errors-container').hide();
 	$('.errors-container .error').html('');
